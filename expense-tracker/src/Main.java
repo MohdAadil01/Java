12cb5045f1cbf72a0e66b4ee24f1f4fbde030208
MohdAadil01 <mohdaadil4240@gmail.com>
2025-01-29 19:33:14 +0530

    complete main.java

--- expense-tracker/src/Main.java
+++ expense-tracker/src/Main.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the expense tracker.");
+        Service svc = new Service();
         Scanner scanner = new Scanner(System.in);
 
         int choice = 2;
@@ -16,7 +19,40 @@ public class Main {
             System.out.print("Choose an option: ");
 
             choice = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter amount: ");
+                    double amount = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.print("Enter category: ");
+                    String category = scanner.nextLine();
+                    System.out.print("Enter description: ");
+                    String description = scanner.nextLine();
+
+                    svc.addExpense(new Expense(amount, category, description, LocalDate.now()));
+                    break;
+                case 2:
+                    svc.viewExpenses();
+                    break;
+                case 3:
+                    String cat = scanner.nextLine();
+                    List<Expense> filteredList = svc.filter(cat);
+                    for (Expense e : filteredList) {
+                        System.out.println(e);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Total Expenses: $" + svc.getTotalExpense());
+                    break;
+                case 5:
+                    System.out.println("Exiting Expense Tracker...");
+                    return;
+
+                default:
+                    System.out.println("Invalid option. Try again.");
+            }
         }
     }
 }
